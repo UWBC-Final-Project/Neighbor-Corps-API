b6e63706752de1de08973c0f3fde7b3f7bdb25d3
thescottbob <scottmcmains@gmail.com>
2019-01-12 13:45:12 -0800

    Added comment schema

--- models/comment.js
+++ models/comment.js
@@ -3,6 +3,9 @@ const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
   createdBy: { type: String, required: true },
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  description: {type: String, required: true},
 });
 
 const Comment = mongoose.model("Comment", commentSchema);
