9447e1767f3ae59444c50c78047d22e7c3125011
Hy Huynh <hyxhuynh@gmail.com>
2019-01-12 01:06:46 -0800

    added initial code for user schema and usersController

--- models/user.js
+++ models/user.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  email: { type: String },
+  phone: { type: String },
+  address: { type: String },
+  meritscore: { type: Number }
 });
 
 const User = mongoose.model("User", userSchema);
