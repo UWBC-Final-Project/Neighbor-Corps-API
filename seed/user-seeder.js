5ef6d69100939bdddc179537b900e80cf8de153d
Kevin <kphyde@gmail.com>
2019-01-13 14:44:57 -0800

    added a couple of NPM packages like body parser, dotenv and a validator suite. Building out seed files too, trying to get it ready to deploy to heroku

--- seed/user-seeder.js
+++ seed/user-seeder.js
@@ -6,13 +6,28 @@ mongoose.connect("mongodb://localhost/neighborCorps");
 
 const users = [
   new User({
-    name: "Pushpinder"
+    username: "UserNumberOne",
+    password: "PasswordOne",
+    email: "User@user.com",
+    phone: "773-555-6059",
+    address: "4122 Main St., Anytown, WA 99999",
+    meritscore: 150
   }),
   new User({
-    name: "Scott"
+    username: "UserNumberTwo",
+    password: "PasswordTwo",
+    email: "User2@user.com",
+    phone: "773-555-4498",
+    address: "122 South St., Anytown, WA 99999",
+    meritscore: 65
   }),
   new User({
-    name: "Hy"
+    username: "UserNumberThree",
+    password: "PasswordThree",
+    email: "User3@user.com",
+    phone: "773-555-5567",
+    address: "55 Doug Rd., Anytown, WA 99999",
+    meritscore: 325
   })
 ];
 
