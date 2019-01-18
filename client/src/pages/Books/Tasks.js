37f86154fd948883e67bc6fe5c823bea0a97cce4
PKaur <dgninder@gmail.com>
2019-01-17 18:38:52 -0800

    deleted extra folder and edited code in tasks.js

--- client/src/pages/Books/Tasks.js
+++ client/src/pages/Books/Tasks.js
@@ -4,7 +4,7 @@ import DeleteBtn from "../../components/DeleteBtn";
 import API from "../../utils/API";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn } from "../../components/TaskForm";
+import { Input, TextArea, FormBtn } from "../../components/Form";
 
 class Tasks extends Component {
   // Setting our component's initial state
