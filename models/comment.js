742dba9466856c1eb8d8f4b05c72078d82027e2a
Kevin <kphyde@gmail.com>
2019-01-17 19:21:44 -0800

    Update Comments Schema for simplify and add belongsToTask

--- models/comment.js
+++ models/comment.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-  description: {type: String, required: true},
-  username: { type: String, unique: true, required: true },
-  tags:[{type: String}],
-  comments: [{body:"string", by: mongoose.Schema.Types.ObjectId}],
+  // description: {type: String, required: true},
+  // username: { type: String, unique: true, required: true },
+  // tags:[{type: String}],
+  comment: [{body:"string", by: mongoose.Schema.Types.ObjectId}],
   postedBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-  postDate: { type: Date, default: Date.now }
+  postDate: { type: Date, default: Date.now },
+  belongsToTask: {type: String}
 });
 
 const Comment = mongoose.model("Comment", commentSchema);
