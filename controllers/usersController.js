255b7f671090440f1f195fd7af5cbaef48ed72a5
Kevin <kphyde@gmail.com>
2019-01-20 15:06:23 -0800

    comment to trail down 422 error

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -17,7 +17,8 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  create: function(req, res) {
+  create: function (req, res) {
+    console.log("toot");
     db.User
       .create(req.body)
       .then(dbModel => res.json(dbModel))
