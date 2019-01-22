499d1bce666e291668deb74287eea88971d50495
Kevin <kphyde@gmail.com>
2019-01-22 10:37:36 -0800

    remove dupliacte declarations that was causing build failure

--- client/src/pages/Books/Tasks.js
+++ client/src/pages/Books/Tasks.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Jumbotron from "../../components/Jumbotron";
 import DeleteBtn from "../../components/DeleteBtn";
-import API from "../../utils/API";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
 import { Input, TextArea, FormBtn } from "../../components/Form";
