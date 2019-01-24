82713edf0807ef2d2935fff1295b106d5f60b1cc
Kevin <kphyde@gmail.com>
2019-01-24 14:58:03 -0800

    update to take comment and belongsToTask datapoints

--- models/comment.js
+++ models/comment.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-  description: {type: String, required: true},
+  comment: { type: String, required: true },
+  belongsToTask: { type: String, required: true },
   username: { type: String, unique: true, required: true },
   tags:[{type: String}],
   // postedBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
